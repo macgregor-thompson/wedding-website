1430e50e43028de32e9f181c12132d67e0669007
MacGregor Thompson <mhthompson86@gmail.com>
2018-01-19 17:17:13 -0500

    made SEVERAL changes and really started getting it together~!

--- js/main.js
+++ js/main.js
@@ -1,15 +1,28 @@
 (function () {
-    'use strict';
+  'use strict';
 
-    window.swapFullImage = function (target) {
-        document.getElementById(target).classList.add('image-loaded');
-    };
+  window.swapFullImage = function (target) {
+    document.getElementById(target).classList.add('image-loaded');
+  };
+
+  $('.no-link').on('click', function (e) {
+    e.preventDefault();
+    return false;
+  });
 
 
-    $('.animateToTop').on('click', function () {
-        $('html, body').animate({
-            scrollTop: 0
-        }, "slow");
-    });
+  $('.back-to-top').on('click', function () {
+    $('html, body').animate({
+      scrollTop: 0
+    }, $(window).scrollTop() / 10);
+  });
+
+
+  $('.scroll-to-section').on('click', function () {
+    var pos = $('#' + $(this).data('section')).offset().top;
+    $('html, body').animate({
+      scrollTop: pos
+    }, 1000);
+  });
 
 })();
